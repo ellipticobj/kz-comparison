78dd611077f0c86d0804115c73d18619e05238df
luna :3 <ellipticobj@gmail.com>
2024-12-27 17:42:28 +0800

    ???? edits

--- funcs.py
+++ funcs.py
@@ -1,7 +1,43 @@
+import logging
+import json
 
-def view():
+
+def parsejson(type):
+    # opens models.json and reads content and decodes it woaow
+    try:
+        with open(f'{type}.json', 'r+') as models:
+            rawcont = models.readlines()
+            cont = json.load(rawcont)
+    except FileNotFoundError:
+        logging.error(f"{type}.json not found")
+        print(f"{type}.json not found. Did you rename it?")
+        makenewjson = input(f"Make empty {type}.json? [y/N] ").strip().lower()
+        if makenewjson == 'y':
+            open(f'{type}.json', 'w+')
+            logging.info(f"empty {type}.json created")
+            logging.info(f"continuing with empty {type}.json")
+        else:
+            logging.warning(f"exiting program due to error: FileNotFoundError")
+            exit()
+    except json.JSONDecodeError:
+        logging.error(f"Error decoding {type}.json")
+        print(f"Error decoding {type}.json. Is the file empty or corrupted?")
+        exit()
+    return cont
+
+def view(cont):
+    choice = input("input models to view: ").lower().strip()
+    
     pass
 
-def compare():
+def compare(cont):
     pass
 
+def add(cont):
+    typetoadd = input("input type to add [e/b/d/q]: ").strip().lower()
+    while typetoadd not in ['e','b','d','q']:
+        typetoadd = input("input type to add [e/b/d/q]: ").strip().lower()
+    name = input("input model name: ")
+    if typetoadd == 'e':
+        pass
+    # TODO
--- main.py
+++ main.py
@@ -4,48 +4,43 @@ from sys import exit
 from classes import *
 from funcs import *
 
+'''
+overcomplicated program for a problem no one has
+'''
+
 print("KZ stats tool")
 
+# debug?
 debug = input("enter to start: ").lower().strip()
 
+# logging init
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='app.log',
                     filemode='a')
-
 if debug == 'verbose' or debug == 'v' or debug == 'debug' or debug == 'd':
     consolehandler = logging.StreamHandler()
     consolehandler.setLevel(logging.DEBUG)
     consolehandler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
     logging.getLogger().addHandler(consolehandler)
 
-try:
-    with open('models.json', 'r+') as models:
-        rawcont = models.readlines()
-        cont = json.load(rawcont)
-except FileNotFoundError:
-    logging.error("models.json not found")
-    print("models.json not found. Did you rename it?")
-    makenewmodelsjson = input("Make empty models.json? [y/N] ").strip().lower()
-    if makenewmodelsjson == 'y':
-        open('models.json', 'w+')
-        logging.info("empty models.json created")
-        logging.info("continuing with empty models.json")
-    else:
-        logging.warning("exiting program due to error: FileNotFoundError")
-        exit()
+
+parsejson("iems")
+parsejson("bluetooth")
+parsejson("dacs")
 
 while True:
     choice = input('''
 1. view stats
 2. compare stats
+3. add item to json
 3. exit
 > ''').strip()
     
     if choice == '1':
-        view()
+        view(cont)
     elif choice == '2':
-        compare()
+        compare(cont)
     elif choice == '3':
         break
     else:
